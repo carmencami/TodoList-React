1714fdbb396d79f741622ab97530f0381e39d4be
carmencami <carmengreta3@outlook.es>
2022-08-10 22:45:21 +0000

    Lack CSS

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,44 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+import React, { useState } from "react";
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const [inputValue, setInputValue]=useState("");
+    const [tasks, setTasks]=useState([]);
+    const handlerInput = (e)=> {
+        setInputValue(e.target.value)
+    }
+    const handleButton = () => {
+        // tasks.push(inputValue)
+        setTasks([...tasks, inputValue]);
+        setInputValue("");
+    }
+    const handlePress = (e) => {
+        // if (e.keyCode == 13){
+        if (e.key == "Enter"){
+            setTasks([...tasks, inputValue]);
+            setInputValue("");
+        }
+    }
+    const resetButton= (index) => {
+        let temp=tasks;
+        tasks.splice(index, 1);
+        setTasks([...temp]);
+    }
+    const html =tasks.map((e, i)=>{
+        return <li key={i}>{e}
+        <button className="btn btn-success" onClick={()=>resetButton(i)}value={tasks}>X</button>
+        </li>
+    })
+    return (
+        <div className="text-center contenedor" >
+            <h1>ToDos</h1>
+            <input className="todo-input form-control" type="text" onChange={handlerInput} onKeyDown={(e)=>handlePress(e)} value={inputValue}/>
+            {/* <button className="btn button-text" onClick={handleButton}></button> */}
+            <ul>
+                {html}
+            </ul>
+        </div>
+    );
 };
-
 export default Home;
+
+
+
